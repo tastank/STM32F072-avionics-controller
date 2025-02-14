d23a28d108108e5c728f1bcce2b6b0b170550686
Tyler Stank <tbear2500@gmail.com>
2025-02-14 01:13:06 -0600

    Reduce update rate so X-Plane does not miss encoder signals

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -371,7 +371,7 @@ static void MX_TIM7_Init(void)
   htim7.Instance = TIM7;
   htim7.Init.Prescaler = 24000-1;
   htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim7.Init.Period = 2-1;
+  htim7.Init.Period = 2*25-1;
   htim7.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim7) != HAL_OK)
   {
--- USB_DEVICE/Target/usbd_conf.h
+++ USB_DEVICE/Target/usbd_conf.h
@@ -76,7 +76,7 @@
 /*---------- -----------*/
 #define USBD_CUSTOM_HID_REPORT_DESC_SIZE     29
 /*---------- -----------*/
-#define CUSTOM_HID_FS_BINTERVAL     0x1
+#define CUSTOM_HID_FS_BINTERVAL     0x05
 
 /****************************************/
 /* #define for FS and HS identification */
